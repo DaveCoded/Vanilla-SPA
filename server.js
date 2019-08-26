92262f8d1b52054189ad8731424f9e04d7837f09
David Bernhard <davidbernhard@live.com>
2019-08-26 13:09:38 +0100

    Get symbols and get conversion rates

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-
 // read .env files
 
 const express = require('express');
-const { getRates } = require('./lib/fixer-service');
+const { getRates, getSymbols } = require('./lib/fixer-service');
+const { convertCurrency } = require('./lib/free-currency-service');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,12 +26,10 @@ const errorHandler = (err, req, res) => {
       .send({ title: 'Server responded with an error', message: err.message });
   } else if (err.request) {
     // The request was made but no response was received
-    res
-      .status(503)
-      .send({
-        title: 'Unable to communicate with server',
-        message: err.message
-      });
+    res.status(503).send({
+      title: 'Unable to communicate with server',
+      message: err.message
+    });
   } else {
     // Something happened in setting up the request that triggered an Error
     res
@@ -51,19 +49,44 @@ app.get('/api/rates', async (req, res) => {
   }
 });
 
-// Redirect all traffic to index.html
+// Fetch Symbols
+app.get('/api/symbols', async (req, res) => {
+  try {
+    const data = await getSymbols();
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+});
 
+// Convert Currency
+app.post('/api/convert', async (req, res) => {
+  try {
+    const { from, to } = req.body;
+    const data = await convertCurrency(from, to);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+});
+
+// Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 // Listen for HTTP requests on port 3000
-
 app.listen(port, () => {
   console.log('listening on %d', port);
 });
 
-// const test = async () => {
-//   const data = await getRates();
-//   console.log(data);
-// };
+const testConversion = async () => {
+  try {
+    const data = await convertCurrency('USD', 'KES');
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// test();
+testConversion();
