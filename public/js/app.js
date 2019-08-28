b08c42008c01f6c89516df0c217dc6ebaec2761d
David Bernhard <davidbernhard@live.com>
2019-08-28 11:10:45 +0100

    change back to localhost

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,7 @@ window.addEventListener('load', () => {
 
   // Instantiate api handler
   const api = axios.create({
-    baseURL: 'https://vanilla-spa.herokuapp.com/api',
+    baseURL: 'http://localhost:3000/api',
     timeout: 5000
   });
 
